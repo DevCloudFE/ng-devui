240f4528b5e62fe8655416b54a9730fea4f25b1c
isanbel <theisanbel@gmail.com>
2020-03-22 21:31:10 +0800

    docs(color-picker): add apart usage demo

---
+++ devui/color-picker/demo/apart/apart.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'd-color-picker-apart',
+  templateUrl: './apart.component.html',
+  styleUrls: ['./apart.component.scss']
+})
+export class ApartComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
