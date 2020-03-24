4ae03b527781b4b0bfcd663b429757469408090f
wangchu <541758229@qq.com>
2020-03-24 23:09:38 +0800

    1

--- devui/button/demo/button-demo.module.ts
+++ devui/button/demo/button-demo.module.ts
@@ -5,15 +5,14 @@ import { FormsModule } from '@angular/forms';
 
 import { ButtonModule } from '../button.module';
 import { ButtonDemoComponent } from './button-demo.component';
+import { StickyModule } from '../../sticky'
+import { AnchorModule } from "../../anchor"
 import { DevUIApiComponent } from 'ng-devui/shared/devui-api/devui-api.component';
 import { CommonComponent } from './common/common.component';
 import { IconComponent } from './icon/icon.component';
 import { LoadingComponent } from './loading/loading.component';
 import { PrimaryComponent } from './primary/primary.component';
 import { TextComponent } from './text/text.component';
-import { CombinationComponent } from './combination/combination.component';
-import { AutofocusComponent } from './autofocus/autofocus.component';
-import { SizeComponent } from './size/size.component';
 import { DevUICodeboxModule } from 'ng-devui/shared/devui-codebox/devui-codebox.module';
 import { DevUIApiModule } from 'ng-devui/shared/devui-api/devui-api.module';
 
@@ -24,6 +23,8 @@ import { DevUIApiModule } from 'ng-devui/shared/devui-api/devui-api.module';
     ButtonModule,
     DevUICodeboxModule,
     DevUIApiModule,
+    StickyModule,
+    AnchorModule,
     RouterModule.forChild([
       { path: '',  redirectTo: 'demo' },
       { path: 'demo', component: ButtonDemoComponent},
@@ -40,9 +41,6 @@ import { DevUIApiModule } from 'ng-devui/shared/devui-api/devui-api.module';
     LoadingComponent,
     PrimaryComponent,
     TextComponent,
-    CombinationComponent,
-    AutofocusComponent,
-    SizeComponent
   ],
   entryComponents: [
     ButtonDemoComponent,
