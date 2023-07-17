a4e3ae3a0da13dbbe5e53f615349ac0cd2ce517e
xu_xz <xxz0528@outlook.com>
2023-07-17 21:31:14 +0800

    feat: 模态弹窗添加最大化和退出最大化功能

---
+++ devui/modal/demo/maximize/maximize.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { DialogService } from 'ng-devui/modal';
+import { ModalTestComponent } from '../modal-test.component';
+
+@Component({
+  selector: 'd-maximize',
+  templateUrl: './maximize.component.html',
+  })
+export class MaximizeComponent {
+  config = {
+    id: 'dialog-service',
+    width: '346px',
+    maxHeight: '600px',
+    title: 'Start Snapshot Version',
+    content: ModalTestComponent,
+    backdropCloseable: true,
+    onClose: () => console.log('on dialog closed'),
+    data: {
+      name: 'Tom',
+      age: 10,
+      address: 'Chengdu',
+    },
+  };
+
+  constructor(private dialogService: DialogService) {}
+
+  openDialog(dialogtype?: string, showAnimation?: boolean) {
+    const results = this.dialogService.open({
+      ...this.config,
+      showMaximizeBtn: true,
+      dialogtype: dialogtype,
+      showAnimation: showAnimation,
+      buttons: [
+        {
+          cssClass: 'primary',
+          text: 'Ok',
+          disabled: false,
+          handler: ($event: Event) => {
+            results.modalInstance.hide();
+          },
+        },
+        {
+          id: 'btn-cancel',
+          cssClass: 'common',
+          text: 'Cancel',
+          handler: ($event: Event) => {
+            results.modalInstance.hide();
+          },
+        },
+      ],
+    });
+    console.log(results.modalContentInstance);
+  }
+}
--- devui/modal/modal-container.component.ts
+++ devui/modal/modal-container.component.ts
@@ -1,10 +1,4 @@
-import {
-  Component,
-  Input,
-  OnInit,
-  TemplateRef,
-  ViewChild
-} from '@angular/core';
+import { Component, Input, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
 import { IButtonStyle } from 'ng-devui/button';
 import { ModalComponent } from './modal.component';
@@ -14,12 +8,13 @@ import { ModalContentDirective } from './modal.directive';
   templateUrl: './modal-container.component.html',
   styleUrls: ['./modal-container.component.scss'],
   preserveWhitespaces: false,
-})
+  })
 export class ModalContainerComponent implements OnInit {
   @Input() maxHeight: string;
   @Input() data: any;
   @Input() title: string;
   @Input() content: string | HTMLElement;
+  @Input() showMaximizeBtn: boolean;
   @Input() buttons: Array<{
     id?: string;
     cssClass?: IButtonStyle;
@@ -31,10 +26,12 @@ export class ModalContainerComponent implements OnInit {
   }>;
   @Input() html: boolean;
   @Input() onClose: ($event?: Event) => void;
+  @Input() onMaximize: (maximized: boolean) => void;
   @ViewChild(ModalContentDirective, { static: true }) modalContentHost: ModalContentDirective;
   @Input() dialogtype = 'standard';
   @Input() showCloseBtn: boolean;
   contentTemplate: TemplateRef<any>;
+  _oldMaxHeight: string;
 
   constructor(private sanitizer: DomSanitizer, public modalInstance: ModalComponent) {}
 
@@ -48,9 +45,21 @@ export class ModalContainerComponent implements OnInit {
     this.onClose(event);
   }
 
+  maximize(maximized: boolean) {
+    if (this.onMaximize) {
+      this.onMaximize(maximized);
+    }
+    if (maximized) {
+      this._oldMaxHeight = this.maxHeight;
+      this.maxHeight = '100vh';
+    } else {
+      this.maxHeight = this._oldMaxHeight;
+    }
+  }
+
   updateButtonOptions(buttonOptions = []) {
     this.buttons = this.buttons.map((button, index) => {
-      return {...button, ...buttonOptions[index]};
+      return { ...button, ...buttonOptions[index] };
     });
   }
 }
