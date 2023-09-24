bf8f4baf012c9243266f21f264e9c36fa5ee5230
ysx <1120805364@qq.com>
2023-09-24 14:26:36 +0800

    feat: menu animation

--- devui/menu/demo/loop-menu/loop-menu.component.ts
+++ devui/menu/demo/loop-menu/loop-menu.component.ts
@@ -2,21 +2,25 @@ import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core
 import { MenuItemType } from 'ng-devui/menu';
 
 
-function findAllParent(data, currentId) {
-  for (let i = 0; i < data.length; i++) {
-    const temp = data[i];
-    if (temp.id === currentId) {
-      this.parentPath.unshift(temp.name);
-      return 1;
-    }
-    if (temp.childOrgVO && temp.childOrgVO.length) {
-      const t = this.setParentPath(temp.childOrgVO, currentId);
-      if (t === 1) {
-        this.parentPath.unshift(temp.name);
-        return 1;
+function findAllParent(source: MenuItemType[], key: string) {
+  const result: string[] = [];
+  const loop = (list: MenuItemType[]) => {
+    for (const item of list) {
+      if (item.key === key) {
+        // parentKeys.unshift(key);
+        return true;
+      }
+      if (item.children?.length) {
+        const t = loop(item.children);
+        if (t) {
+          result.unshift(item.key);
+          return true;
+        }
       }
     }
-  }
+  };
+  loop(source);
+  return result;
 }
 
 
@@ -25,9 +29,14 @@ function findAllParent(data, currentId) {
   template: `
     <div d-menu [collapsed]="collapsed">
       <ng-container *ngFor="let item of menus; trackBy: trackByMenu">
-        <loop-sub-menu [menu]="item" [openKeys]="openKeys" [activeKey]="activeKey" *ngIf="item.children?.length; else leafTpl" />
+        <loop-sub-menu
+          [menu]="item"
+          [openKeys]="openKeys"
+          [disabledKeys]="disabledKeys"
+          [activeKey]="activeKey"
+          *ngIf="item.children?.length; else leafTpl" />
         <ng-template #leafTpl>
-          <div d-menu-item (itemClick)="itemClick(item.key)" [active]="item.key === activeKey">
+          <div d-menu-item (itemClick)="itemClick(item.key)" [disabled]="disabledKeys.includes(item.key)" [active]="item.key === activeKey">
             <d-icon class="devui-menu-item-icon" *ngIf="item.icon" [icon]="item.icon" />
             <span class="devui-menu-item-name over-flow-ellipsis">{{ item.name }}</span>
           </div>
@@ -41,10 +50,18 @@ export class LoopMenuComponent implements OnInit {
   @Input() collapsed = false;
   @Input() menus: MenuItemType[] = [];
 
-  openKeys: string[] = ['c-3', 'c-3-1'];
-  activeKey = 'c-3-1-1';
+  // disabledKeys = [];
+  disabledKeys = ['c-1', 'c-2', 'c-3-1'];
 
-  ngOnInit() { }
+  activeKey = 'c-3-1-1';
+  openKeys: string[] = [];
+  // openKeys: string[] = ['c-3', 'c-3-1'];
+
+
+  ngOnInit() {
+    this.openKeys = findAllParent(this.menus, 'c-3-1-1');
+    // console.log('openKeys', this.openKeys);
+  }
 
   /* openChange(open: boolean, key: string) {
     if (open) {
