e63d68e2b527d2707f333e860941573d2f91e685
Simplelegant <466033631@qq.com>
2023-11-06 22:41:37 +0800

    fix(menu): 指令、标签、host规范化

--- devui/menu/demo/loop/loop-menu/loop-sub-menu.component.ts
+++ devui/menu/demo/loop/loop-menu/loop-sub-menu.component.ts
@@ -2,16 +2,16 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnChanges, OnI
 import { MenuItemType } from 'ng-devui/menu';
 
 @Component({
-  selector: 'loop-sub-menu',
+  selector: 'd-loop-sub-menu',
   template: `
     <div
-      d-sub-menu
+      dSubMenu
       [title]="menu.name"
       [icon]="menu.icon">
       <ng-container *ngFor="let item of menu.children; trackBy: trackByMenu">
-        <loop-sub-menu [menu]="item" [activeKey]="activeKey" (itemClick)="onClick($event)" *ngIf="item.children?.length; else leafTpl" />
+        <d-loop-sub-menu [menu]="item" [activeKey]="activeKey" (itemClick)="onClick($event)" *ngIf="item.children?.length; else leafTpl" />
         <ng-template #leafTpl>
-          <div d-menu-item [active]="activeKey === item.key" (itemClick)="onClick(item.key)">
+          <div dMenuItem [active]="activeKey === item.key" (itemClick)="onClick(item.key)">
             <d-icon class="devui-menu-item-icon" *ngIf="item.icon" [icon]="item.icon" />
             <span class="devui-menu-item-name over-flow-ellipsis">{{ item.name }}</span>
           </div>
